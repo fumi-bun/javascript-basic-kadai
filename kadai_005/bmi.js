a9c8a3ee8b457be721eeb1ae7a3e63353aa0be73
fumi-bun <150202074+fumi-bun@users.noreply.github.com>
2023-12-01 23:06:53 +0900

    kadai_005をアップロードしました。

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 定数の宣言・値の代入
+const weight = 68;
+
+// コンソールへの出力
+console.log(weight);
+
+// 値の再代入
+const height = 1.7;
+
+// コンソールへの出力
+console.log(height);
+
+// 値の再代入
+const bmi=(68/(1.7*1.7));
+
+// コンソールへの出力
+console.log(bmi);
